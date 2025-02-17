9693bbe37558062f9d8eaaa9fdee8229798da287
Linux-user <code2time@gmail.com>
2025-02-17 08:23:13 +0330

    Cretae Basic Button Component

---
+++ components/Common/Button.vue
@@ -0,0 +1,186 @@
+<template>
+	<v-btn
+		:color="color"
+		:disabled="disabled"
+		:outlined="outlined"
+		:text="text"
+		:rounded="rounded"
+		:icon="icon"
+		:loading="loading"
+		:block="block"
+		:depressed="depressed"
+		:fab="fab"
+        :large="large"
+        :small="small"
+        :x-large="Xlarge"
+        :x-small="Xsmall"
+        :to="to"
+        :href="href"
+        :dark="dark"
+        :light="light"
+        :elevation="elevation"
+        :ripple="ripple"
+        :type="type"
+        :width="width"
+        :max-width="maxWidth"
+        :min-width="minWidth"
+        :height="height"
+        :max-height="maxHeight"
+        :min-height="minHeight"
+        :value="value"
+        :active-class="activeClass"
+        :exact="exact"
+        :exact-active-class="exactActiveClass"
+        :exact-path="exactPath"
+        :absolute="absolute"
+        :append="append"
+        :bottom="bottom"
+        :fixed="fixed"
+        :input-value="inputValue"
+        :left="left"
+        :right="right"
+        :link="link"
+        :nuxt="nuxt"
+        :plain="plain"
+	>
+		{{value}}
+	</v-btn>
+</template>
+<script>
+export default {
+	name: 'customButton',
+	props: {
+		color: {
+			type: String,
+			default: 'primary',
+		},
+		disabled: {
+			type: Boolean,
+		},
+		outlined: {
+			type: Boolean,
+            default : true
+		},
+		text: {
+			type: Boolean,
+		},
+		rounded: {
+			type: Boolean,
+		},
+		icon: {
+			type: Boolean,
+		},
+		loading: {
+			type: Boolean,
+		},
+		block: {
+			type: Boolean,
+		},
+		depressed: {
+			// show button without any shadow
+			type: Boolean,
+		},
+		fab: {
+			type: Boolean,
+		},
+        large : {
+            type : Boolean
+        },
+        small : {
+            type : Boolean
+        },
+        Xlarge : {
+            type : Boolean
+        },
+        Xsmall : {
+            type : Boolean
+        },
+        to :{
+            type : [String || Object]
+        } ,
+         href :{
+            type : [String || Object]
+        },
+        dark :{
+            type : Boolean
+        },
+        light :{
+            type : Boolean
+        },
+        elevation :{
+            type : [Number || String]
+        },
+        ripple :{     //(ripple effect
+            type : [Boolean , Object],
+        },
+        type : {
+            type : String
+        },
+        with :{
+            type : String
+        },
+        minWidth :{
+            type : String
+        },
+        maxWidth :{
+            type : String
+        },
+        height :{
+            type : String
+        },
+        minHeight :{
+            type : String
+        },
+        maxHeight :{
+            type : String
+        },
+        value :{
+            type : [String || Number]
+        },
+        activeClass : {
+            type : String
+        },
+        exact : {
+            type : Boolean
+        },
+        exactActiveClass :{
+            type : String
+        },
+        exactPath : {
+            type : Boolean
+        },
+        absolute :{
+            type : Boolean
+        },
+        append :{    // append link
+            type : Boolean
+        },
+        bottom :{  
+            type : Boolean
+        },
+        fixed : {
+            type  : Boolean
+        },
+        inputValue : {
+            type : [String || Number || Object || Array]
+        },
+        left :{
+            type : Boolean
+        },
+        right :{
+            type : Boolean
+        },
+        link :{
+            type : Boolean
+        },
+        nuxt : {   // use vue router || use Spa
+            type : Boolean
+        },
+        plain : {  // remove background color in hover
+            type  : Boolean,
+        },
+
+	},
+}
+</script>
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,14 +5,20 @@
 				{{ $t('HomePage.HomeTitle') }}
 			</h1>
 			<h3 class="poppins-2">{{ $t('HomePage.Home_Discription') }}</h3>
-			<div class="d-flex flex-column justify-center align-center pa-3"></div>
+			<div class="d-flex flex-column justify-center align-center pa-3">
+				<Button value="دکمه تستی"/>
+			</div>
 		</div>
 	</v-app>
 </template>
 
 <script>
+import Button from '~/components/Common/Button.vue'
 export default {
 	name: 'IndexPage',
+	components : {
+		Button
+	}
 }
 </script>
 
